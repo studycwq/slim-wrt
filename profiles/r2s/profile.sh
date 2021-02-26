96dd0ff6972b01fee91fa2b549bf4aeb907ddc82
Will Chen <studycwq@gmail.com>
2021-02-26 22:04:17 +0800

    feat: r2s use master branch

--- profiles/r2s/profile.sh
+++ profiles/r2s/profile.sh
@@ -2,8 +2,3 @@
 
 SLIM_CFG_TARGET="rockchip"
 SLIM_CFG_ARCH="armv8"
-
-# r2s support in master branch
-SLIM_CFG_OPENWRT_BRANCH="master"
-# let commit code empty to follow the latest
-SLIM_CFG_OPENWRT_COMMIT=""
